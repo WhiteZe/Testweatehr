17e9e99719ad80975661eb7bce87fcc6875ffbb3
WhiteZe <acoldnight@qq.com>
2017-06-12 14:23:20 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/weather/administrator/weather/MyWeatherActivity.java
+++ app/src/main/java/com/weather/administrator/weather/MyWeatherActivity.java
@@ -1,40 +1,36 @@
 package com.weather.administrator.weather;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class MyWeatherActivity extends AppCompatActivity {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.add_item:
-                Toast.makeText(this,"You Clicked Add",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.remove_item:
-                Toast.makeText(this,"You Clicked Remove",Toast.LENGTH_SHORT).show();
-                break;
-            default:
-        }
-        return  true;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_weather);
+        //初始化各控件
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather",null) != null){
+            Intent intent = new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
     }
+
 }
---
+++ app/src/main/java/com/weather/administrator/weather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.weather.administrator.weather.gson;
+
+/**
+ * Created by Administrator on 2017/6/9.
+ */
+
+public class AQI {
+    public AQICITY city;
+    public class AQICITY{
+        public String aqi;
+        public String pm25;
+    }
+}
